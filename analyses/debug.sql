f45c1e1ce9738efa9efb5696e12f4f2a854dd25a
AleksandrMazurkevich <36474711+AleksandrMazurkevich@users.noreply.github.com>
2025-02-11 21:52:55 +0300

    homework

---
+++ analyses/debug.sql
@@ -0,0 +1,6 @@
+select
+    "ticket_no",
+    "flight_id",
+    "fare_conditions",
+    "amount"
+from "dwh_flight"."demo_src"."ticket_flights"
--- models/staging/flights/stg_ticket_flights.sql
+++ models/staging/flights/stg_ticket_flights.sql
@@ -1,15 +1,17 @@
-{{
-    config (
-        materialized = 'table',
-        
-    )
+{{ 
+    config(
+        materialized='table',
+    ) 
 }}
 
-    select
-        "ticket_no",
-        "flight_id",
-        "fare_conditions",
-        "amount"
+select
+    "ticket_no",
+    "flight_id",
+    "fare_conditions",
+    "amount"
+from {{ source('demo_src', 'ticket_flights') }}
 
-    from {{ source('demo_src', 'ticket_flights')}}
+{%- if target.name == 'dev' %}
+limit 300
+{% endif %}
   
